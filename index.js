8b4bbd2018b641d5626eb3b361b65e44dab9d0ae
Aengus <aengus.2000@gmail.com>
2022-07-07 01:17:32 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const messages =[]
+function writeCards(name) {
+   for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);   
+    };
+    return (messages)
+};
+function countDown() {
+    let countdown = 10;
+    while (countdown > -1) {
+        console.log(countdown--);
+    }
+};
